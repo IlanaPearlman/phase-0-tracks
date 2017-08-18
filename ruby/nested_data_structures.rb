764cd351ca831a51cfb08b23168241585de59ee7
Ilana Pearlman <ilanapearlman@gmail.com>
2017-08-17 21:55:31 -0700

    write road nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,60 @@
+highway = {
+    lane_1: {
+        current_accident: false,
+        qualities: {
+            material: "aspault",
+            pot_holes: 7,
+            needs_resurfacing: true,
+        },
+        vehicles: [
+            {car_1: ["Sam","Jim"]
+            },
+            {car_2: ["Karen"]
+            },
+        ]
+    },
+    lane_2: {
+        current_accident: true,
+        qualities: {
+            material: "concrete",
+            pot_holes: 1,
+            needs_resurfacing: true,
+        },
+        vehicles: [
+            {car_1: ["Frank"]
+            },
+            {car_2: ["James", "Jamier", "Jamiest"]
+            },
+        ]
+    },
+    lane_3: {
+        current_accident: false,
+        qualities: {
+            material: "concrete",
+            pot_holes: 0,
+            needs_resurfacing: false,
+        },
+        vehicles: [
+            {car_1: ["Kim"]
+            },
+            {car_2: ["Christian", "Kathy", "JP"]
+            },
+            {car_3: ["Christian", "Kathy", "JP"]
+            },
+            {car_4: ["Christian", "Kathy", "JP"]
+            },
+        ]
+    }
+    lane_4: {
+        current_accident: True,
+        qualities: {
+            material: "concrete",
+            pot_holes: 3,
+            needs_resurfacing: true,
+        },
+        vehicles: [
+            {car_1: ["Barry"]
+            }
+        ]
+    }
+}
