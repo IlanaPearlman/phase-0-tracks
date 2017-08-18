61d2078087f509df3bdc9623b3dc09eabbe48704
Ilana Pearlman <ilanapearlman@gmail.com>
2017-08-17 22:44:26 -0700

    did release 0

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,33 @@
+=begin
+In phase-0-tracks/ruby/alias_manager.rb, pseudocode and write a method that takes a spy's real name (e.g., "Felicia Torres") and creates a fake name with it by doing the following:
+
+Swapping the first and last name.
+Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+=end
+
+def fake_name(name)
+  #switch first and last
+  swapped = swap(name)
+  #Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet.
+  swapped.chars.inject([]) do |array, char|
+    if 'aeiou'.include?(char)
+      array << vowel_sub(char)
+    elsif char == ' '
+      array << char
+    else
+      array << char.next
+    end
+    array
+  end.join('')
+end
+
+def swap(name)
+  name.split(' ').reverse.join(' ')
+end
+
+def vowel_sub(char)
+  index  = 'aeiou'.index(char)
+  index <= 4 ? 'aeiou'[index + 1] : 'a'
+end
+
+p fake_name("Felicia Torres")
