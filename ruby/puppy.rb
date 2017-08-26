d8d0f9b699cfae5afde33def721e519fec4d162b
Ilana Pearlman <ilanapearlman@gmail.com>
2017-08-25 22:59:26 -0700

    add new trick

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+=begin
+
+SPECIES ------------------------
+canine
+
+CHARACTERISTICS ----------------
+leg number: 4
+hair length: varies
+age: varies
+list of last eaten items: an array
+
+BEHAVIOR -----------------------
+sleep
+pee
+poop
+eat
+run
+sleep
+play
+sit
+
+end
