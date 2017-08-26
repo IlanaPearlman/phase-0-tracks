3f987292073ecc84f35947fd8106d9d6bd177e64
Ilana Pearlman <ilanapearlman@gmail.com>
2017-08-26 12:15:47 -0700

    add mixin version of Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 
   def self.yell_angrily(words)
@@ -15,3 +16,31 @@ end
 p Shout.yell_angrily("bummer")
 
 p Shout.yell_hapily("yay")
+=end
+
+#mixin version of shout module
+module Shout
+
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yell_hapily(words)
+    words + "!" + " :) :) :)"
+  end
+
+end
+
+class Team
+  include Shout
+end
+
+class ElementarySchoolRoom
+  include Shout
+end
+
+warriors = Team.new
+p warriors.yell_hapily("we won")
+
+first_grade = ElementarySchoolRoom.new
+p first_grade.yell_angrily("recess is over")
