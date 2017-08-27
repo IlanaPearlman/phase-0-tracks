b28ba80fcf7739b50d728437c48e3d9851fc6484
Ilana Pearlman <ilanapearlman@gmail.com>
2017-08-27 14:53:21 -0700

    pass rspec tests

--- ruby/game_class_solo_challenge_6.7/game.rb
+++ ruby/game_class_solo_challenge_6.7/game.rb
@@ -6,6 +6,17 @@ The guessing player receives continual feedback on the current state of the word
 The user should get a congratulatory message if they win, and a taunting message if they lose.
 =end
 
+#method to allow testing of IO stream
+  def capture_standard_output(&block)
+    original_stream = $stdout
+    $stdout = mock = StringIO.new
+    yield
+    mock.string.chomp
+  ensure
+    $stdout = original_stream
+  end
+
+
 class Game
 
   attr_reader :secret, :clue, :past_guesses, :remaining_guesses
@@ -13,18 +24,21 @@ class Game
 #calls a string getting method and stores the string in an instance variable
 #stores instance variable clue for the guess string, initially all "_", of same length as other instance string
 #stores list of guessed letters in array, initially empty
-  def initialize(secret = get_secret_to_guess)
+  def initialize(secret = "default")
     @secret = secret
-    @clue = "_" * @secret.length
     @past_guesses = []
-    @remaining_guesses = @secret.length
   end
 
 #initial string method
-#returns instance variable for initial string
+#returns instance variable for initial string, updates inital values for clue and remaining guesses
   def get_secret_to_guess
-    puts "What is the secret to be guessed?"
-    gets.chomp
+    puts "Player 1: what is the secret to be guessed?"
+    @secret = gets.chomp
+  end
+
+  def set_initial_clue_and_remaining_guesses
+    @clue = "_" * @secret.length
+    @remaining_guesses = @secret.length
   end
 
 #guessing inteface method
@@ -33,9 +47,10 @@ class Game
 # loop until guess input valid
 #updates current clue if guessed letter is included
   def guess_character
+    puts "Player 2: You now have #{@remaining_guesses} guesses"
     guess = ''
+    puts "Guess a letter. The clue is #{@clue}."
     loop do
-      puts "Guess a letter. The clue is #{@clue}."
       guess = gets.chomp
       break if valid_guess(guess)
       puts "Try again! It must be a single character that was not guessed yet."
@@ -80,17 +95,29 @@ class Game
     won? || !more_guesses_remaining
   end
 
+  def congratulatory_message
+    "WINNER! It was '#{@secret}'!"
+  end
+
+  def taunting_message
+    "NOPE, it was '#{@secret}'. You lost :("
+  end
+
 # game play method
 #calls guessing interface once per each letter of the initial string
 #congratulatory message if they win, and a taunting message if they lose.
 
   def play
+    get_secret_to_guess
+    set_initial_clue_and_remaining_guesses
     while !over?
       guess_character
     end
-    puts "#{won? ? "Yay! It was #{@secret}!" : "Tragedy! Game over:("}"
+    puts "#{won? ? congratulatory_message: taunting_message}"
   end
 end
 
-my_game = Game.new
-my_game.play
+if __FILE__ == $PROGRAM_NAME
+  new_game = Game.new
+  new_game.play
+end
--- ruby/game_class_solo_challenge_6.7/game_spec.rb
+++ ruby/game_class_solo_challenge_6.7/game_spec.rb
@@ -6,5 +6,44 @@ The guessing player receives continual feedback on the current state of the word
 The user should get a congratulatory message if they win, and a taunting message if they lose.
 =end
 
-require_relative "game"
+require_relative "game.rb"
+
+
+
+describe Game do
+
+    let(:new_game){Game.new("apple")}
+
+    it "is an instance of Game class" do
+      expect(:new_game).to be_instance_of Game
+    end
+
+    it "first user can enter a string to be guessed" do
+      output = capture_standard_output { :new_game.get_secret_to_guess }
+      expect(output).to eq "Player 1: what is the secret to be guessed?"
+      game.input.stub(:gets.chomp) { 'apples' }
+      expect(game.secret).to eq 'apples'
+    end
+
+    it "second user attepts to guess the string, and recieves feedback on the current state of the word" do
+      output = capture_standard_output { :new_game.guess_character }
+      expect(output).to eq "Player 2: You now have #{:new_game.remaining_guesses} guesses:\nGuess a letter. The clue is _______."
+      :new_game.input.stub(:gets.chomp) { 'd' }
+      expect(:new_game.clue).to eq 'd______'
+    end
+
+    it "number of guesses is limited, and related to length of word" do
+      expect(:new_game.remaining_guesses).to be <= (:new_game.secret.length)
+    end
+
+    it "user should get a congratulatory message if they win" do
+      expect(:new_game.congratulatory_message).to eq     "WINNER! It was '#{:new_game.secret}'!"
+    end
+
+    it "user should get a taunting message if they lose" do
+      expect(:new_game.taunting_message).to eq "NOPE, it was '#{:new_game.secret}'. You lost :("
+    end
+
+end
+
 
