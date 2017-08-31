ad21058f03eeb9201697d8f48ae8146f0a48ca5a
Ilana Pearlman <ilanapearlman@gmail.com>
2017-08-30 21:09:02 -0700

    two arrays

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+var fourColors = ["maroon", "teal", "true-blue", "salmon"];
+var fourNames = ["killer","Demon", "Biter", "Spot"];
+
+fourNames.push("Anne");
+fourColors.push("Pink");
+console.log(fourNames + " " + fourColors);
