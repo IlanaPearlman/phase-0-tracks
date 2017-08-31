fba2d57e1f151149334f0abb3ccb668606d6bca4
Ilana Pearlman <ilanapearlman@gmail.com>
2017-08-30 21:20:51 -0700

    build an object

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,9 @@ var fourNames = ["killer","Demon", "Biter", "Spot"];
 fourNames.push("Anne");
 fourColors.push("Pink");
 console.log(fourNames + " " + fourColors);
+
+object = {};
+for (i = 0; i < fourNames.length; i++ ){
+  object[fourNames[i]] = fourColors[i];
+}
+ console.log(object);
