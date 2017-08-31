adcfbe744c4db5580a8797adecdeb38adbf4da92
Ilana Pearlman <ilanapearlman@gmail.com>
2017-08-30 20:42:44 -0700

    wrote string reverse function

---
+++ js/explore.js
@@ -0,0 +1,17 @@
+/*
+split the string into an array
+reverse the array
+join the array into a string
+*/
+
+function string_reverse(string) {
+  return string.split('').reverse().join('');
+}
+
+/*
+loop through the string
+
+
+*/
+
+console.log(string_reverse('pants'));
