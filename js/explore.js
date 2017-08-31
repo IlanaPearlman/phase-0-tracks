99f8ed3f0a50fa59d3deea2f165852b298958ebc
Ilana Pearlman <ilanapearlman@gmail.com>
2017-08-30 20:52:03 -0700

    add another reverse method

--- js/explore.js
+++ js/explore.js
@@ -4,14 +4,26 @@ reverse the array
 join the array into a string
 */
 
-function string_reverse(string) {
+function stringReverse(string) {
   return string.split('').reverse().join('');
 }
 
+console.log(stringReverse('pants'));
+
 /*
-loop through the string
-
-
+make empty new string
+loop through the string from the back
+take each letter and put it into a new string
+return the new string
 */
 
-console.log(string_reverse('pants'));
+function anotherStringReverse(string){
+  var reversed = ''
+  for (var i = string.length - 1; i >= 0; i--) {
+    console.log(reversed);
+    reversed = reversed + string[i];
+  }
+  return reversed;
+}
+
+console.log(anotherStringReverse("more"));
